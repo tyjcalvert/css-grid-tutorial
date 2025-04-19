7c0f809712cfc0aeb8bfc970b018b22b2b9abeb2
Tyler Calvert <tyjcalvert@gmail.com>
2025-04-18 23:01:35 -0500

    added photos, playing with basic photo component, probably to change slightly later

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,5 +1,16 @@
 import "./PhotoGallery.css";
 
 export default function PhotoGallery() {
-  
+  return (
+    <section className="photo-gallery">
+      {Array.from({ length: 12 }, (_, i) => (
+        <div key={`photo-${i}`} className="card photo-card">
+          <img
+            src={`https://picsum.photos/200/200?random=${i}`}
+            alt={`Random ${i}`}
+          />
+        </div>
+      ))}
+    </section>
+  )
 }
