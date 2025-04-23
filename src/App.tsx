73678385a6a96e8859d0065126a8a8899dd2b86a
Tyler Calvert <tyjcalvert@gmail.com>
2025-04-22 21:49:27 -0500

    adding basic layout for displaying different components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-// import { useState } from 'react'
-import BasicGrid from './components/BasicGrid'
-import './App.css'
+import { useState } from "react";
+import BasicGrid from "./components/BasicGrid";
+import FancyGrid from "./components/FancyGrid";
+import PhotoGallery from "./components/PhotoGallery";
+import "./App.css";
 
 function App() {
-  
+  const [componentIndex, setComponentIndex] = useState(0);
+  const components = [<BasicGrid />, <FancyGrid />, <PhotoGallery />];
 
   return (
     <>
-      <BasicGrid />
+      <main>
+        <h1>CSS Grid Tutorial</h1>
+        <nav>
+          <ul>
+            <li>
+              <button onClick={() => setComponentIndex(0)}>Basic Grid</button>
+            </li>
+            <li>
+              <button onClick={() => setComponentIndex(1)}>Fancy Grid</button>
+            </li>
+            <li>
+              <button onClick={() => setComponentIndex(2)}>
+                Photo Gallery
+              </button>
+            </li>
+          </ul>
+        </nav>
+        <div className="container">{components[componentIndex]}</div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FancyGrid.tsx
+++ src/components/FancyGrid.tsx
@@ -1,5 +1,5 @@
 import "./FancyGrid.css";
 
 export default function FancyGrid() {
-
+  return <div></div>;
 }
